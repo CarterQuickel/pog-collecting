d59520af92762747f561c4ac93094de07bdae9b3
CarterQuickel <27CQuickel@live.ytech.edu>
2025-09-24 06:34:44 -0700

    Update rarities.js

--- JSGambling/static/js/rarities.js
+++ JSGambling/static/js/rarities.js
@@ -5,63 +5,63 @@
         color: "gray",
         income: 4,
         chance: 0.5,
-        value: 25
+        value: 62
       },
       {
         name: "Uncommon",
         color: "green",
         income: 13,
         chance: 0.3,
-        value: 50
+        value: 88
       },
       {
         name: "Rare",
         color: "lightblue",
         income: 27,
         chance: 0.15,
-        value: 100
+        value: 197
       },
       {
         name: "SR",
         color: "blue",
         income: 34,
         chance: 0.1,
-        value: 150
+        value: 352
       },
       {
         name: "Epic",
         color: "purple",
         income: 41,
         chance: 0.06,
-        value: 250
+        value: 490
       },
       {
         name: "SSR",
         color: "yellow",
         income: 67,
         chance: 0.025,
-        value: 500
+        value: 633
       },
       {
         name: "SSSR",
         color: "pink",
         income: 169,
         chance: 0.008,
-        value: 1000
+        value: 1856
       },
       {
         name: "Legendary",
         color: "orange",
         income: 420,
         chance: 0.0045,
-        value: 5000
+        value: 13204
       },
       {
         name: "Mythical",
         color: "red",
         income: 911,
         chance: 0.0003,
-        value: 10000
+        value: 45079
       },
       {
         name: "GODLY",
