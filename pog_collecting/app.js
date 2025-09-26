190fb34b5e6e207b6ea1457c78d2fc85569f68b3
Vincent Liu <157391440+VamyVamizy@users.noreply.github.com>
2025-09-26 07:46:16 -0700

    Update app.js

--- pog_collecting/app.js
+++ pog_collecting/app.js
@@ -63,7 +63,7 @@ const db = new sqlite3.Database('./db/scores.db', (err) => {
 
 // home page
 // change / to /collection when using formbar authentication
-app.get('/', (req, res) => {
+app.get('/collections', (req, res) => {
     res.render('collection');
 });
 
