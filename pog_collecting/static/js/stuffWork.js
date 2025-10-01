06568c7badadd8983ce7e9516a347b94ee0f1faa
CarterQuickel <27CQuickel@live.ytech.edu>
2025-10-01 11:16:19 -0700

    yes

--- pog_collecting/static/js/stuffWork.js
+++ pog_collecting/static/js/stuffWork.js
@@ -47,7 +47,7 @@ function getTotalIncome() {
 // initial money display
 setInterval(updateMoney, 100);
 function updateMoney() {
-    abbreviatedMoney = abbreviateNumber(money);
+    const abbreviatedMoney = abbreviateNumber(money);
     document.getElementById("money").innerText = `$${abbreviatedMoney}`;
 }
 
@@ -68,12 +68,14 @@ function sellItem(index) {
 // update loop
 setInterval(update, 100);
 function update() {
-
+//abbrevs
+    const abbreviatedXP = abbreviateNumber(xp);
+    const abbreviatedMaxXP = abbreviateNumber(maxXP);
     // update inventory size text
     document.getElementById("invTxt").innerHTML = `${inventory.length}/${Isize} Slots`
 
     // update XP Txt
-    document.getElementById("XPTxt").innerText = `Level ${level} (${xp}/${maxXP} XP)`;
+    document.getElementById("XPTxt").innerText = `Level ${level} (${abbreviatedXP}/${abbreviatedMaxXP} XP)`;
 
     // update income Txt
     document.getElementById("income").innerText = `($${getTotalIncome()}/s)`;
@@ -206,6 +208,8 @@ document.getElementById("save").addEventListener("click", () => {
 });
 
 document.getElementById("patchNotesButton").addEventListener("click", () => {
+const pnb = confirm("Save data before going to patch notes?");
+if (pnb) {
     const saveState = {
         money: money,
         inventory: inventory,
@@ -215,6 +219,9 @@ document.getElementById("patchNotesButton").addEventListener("click", () => {
         level: level
     };
     localStorage.setItem("gameState", JSON.stringify(saveState));
+    alert("Game Saved!");
+    window.location.href = "/patchNotes";
+}
 });
 
 // load game
