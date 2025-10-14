fede8f88a9bcd0885c118fe4951d282b5832dbaa
CarterQuickel <27CQuickel@live.ytech.edu>
2025-10-14 05:41:24 -0700

    t

--- pog_collecting/static/js/stuffWork.js
+++ pog_collecting/static/js/stuffWork.js
@@ -199,7 +199,7 @@ function levelup() {
         xp -= maxXP;
         level++;
         maxXP = Math.floor(maxXP * 1.67);
-        Isize += 3;
+        Isize += level % 5 === 0 ? 10 : 5; // increase inventory size by 10 every 5 levels, otherwise by 5
     }
 }
 
