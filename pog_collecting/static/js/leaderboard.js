da1c51c6cc7f2b213c1db439f5d9d2289e7cf3a2
CarterQuickel <27CQuickel@live.ytech.edu>
2025-10-29 14:56:07 -0400

    x

--- pog_collecting/static/js/leaderboard.js
+++ pog_collecting/static/js/leaderboard.js
@@ -1,6 +1,5 @@
 var userdata = JSON.parse(document.getElementById("userdata").textContent);
 var scores = JSON.parse(document.getElementById("scores").textContent);
-console.log(scores)
 
 //mode
 if (userdata.theme === "light") {
@@ -15,8 +14,18 @@ document.querySelectorAll(".infobtn").forEach(button => {
     button.addEventListener('click', () => {
         const index = button.dataset.index;
         const player = scores[index]
-        const inv = player.inventory
-        console.log(player)
-        alert(`Player: ${player.displayname}\nScore: ${player.score}\nLevel: ${player.level}\nPogs: ${player.pogamount}\nXP: ${player.xp}\nInventory: ${JSON.parse(JSON.stringify(inv))}`);
+        const inv = JSON.parse(player.inventory)
+        console.log(inv)
+        let sortedI = inv.map((item => ` ${item.name}`));
+        displayed = sortedI.slice(0, 100);
+        if (sortedI.length > 100) {
+            displayed.push(" ...and more");
+        }
+        console.log(sortedI)
+        const info = document.getElementById("details");
+        info.innerHTML = `<h3>Details for ${player.displayname}</h3>
+                          <h4>Score</h4> <p>${player.score}</p>
+                          <h4>Level</h4> <p>${player.level}</p>
+                          <h4>Inventory</h4> <p>${sortedI.length > 0 ? displayed : "No items"}</p>`;
     })
 });
