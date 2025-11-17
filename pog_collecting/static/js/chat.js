6d534245ab039d10813c3d5f3dd99f4669b4b8c0
Vincent Liu <157391440+VamyVamizy@users.noreply.github.com>
2025-11-17 14:55:59 -0500

    fix lightmode

--- pog_collecting/static/js/chat.js
+++ pog_collecting/static/js/chat.js
@@ -4,10 +4,14 @@ var userdata = JSON.parse(document.getElementById("userdata").textContent);
 if (userdata.theme === "light") {
     document.body.style.backgroundColor = "white";
     document.body.style.color = "black";
+    document.getElementById("message").style.backgroundColor = "black";
+    document.getElementById("message").style.color = "white";
 } else if (userdata.theme === "dark") {
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
 }
+
+
 
 const chatInput = document.getElementById('messageInput');
 const mesasageCont = document.getElementById('messageCont');
