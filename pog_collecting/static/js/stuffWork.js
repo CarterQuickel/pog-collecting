a8ce98f064d26439c4b95c6fa43fb2d1ddeb2921
CarterQuickel <27CQuickel@live.ytech.edu>
2025-12-04 10:19:48 -0500

    pfp

--- pog_collecting/static/js/stuffWork.js
+++ pog_collecting/static/js/stuffWork.js
@@ -739,16 +739,32 @@ document.getElementById("closePfpChanger").addEventListener("click", () => {
 });
 
 document.getElementById("filepfp").addEventListener("change", () => {
+    const fileSizeMessage = document.getElementById('fileSizeMessage');
+    const maxFSKB = 64
+    const maxFSByte = maxFSKB * 1000 // kilobytes in bytes
     const file = document.getElementById("filepfp").files[0];
-    if (!file) return;
-    const reader = new FileReader();
-    reader.onloadend = function () {
-        pfpimg = reader.result;
-        document.getElementById("userPic").src = pfpimg;
-        document.getElementById("bigpfp").src = pfpimg;
-    }
-    if (file) {
-        reader.readAsDataURL(file);
+    const fileInput = document.getElementById("filepfp");
+    if (fileInput.files.length > 0) {
+        if (file.size > maxFSByte) {
+            fileSizeMessage.textContent = `File must be smaller than ${maxFSKB}KB`;
+            fileInput.value = '';
+            fileSizeMessage.style.color = "red";
+        } else {
+            fileSizeMessage.textContent = `File size: ${(file.size / (1000)).toFixed(2)}KB`;
+            fileSizeMessage.style.color = 'green';
+            if (!file) return;
+            const reader = new FileReader();
+            reader.onloadend = function () {
+                pfpimg = reader.result;
+                document.getElementById("userPic").src = pfpimg;
+                document.getElementById("bigpfp").src = pfpimg;
+            }
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        }
+    } else {
+        fileSizeMessage.textContent = '';
     }
 });
 
