d5da8971d71bb593f1d806b61b37642e0bdb6e52
CarterQuickel <27CQuickel@live.ytech.edu>
2025-12-08 08:47:02 -0500

    sa

--- pog_collecting/app.js
+++ pog_collecting/app.js
@@ -18,7 +18,7 @@ const API_KEY = 'c44f8bc4bb6acef2b313cdb70ddae3228bf0cb73015831fe9133b1dd2c758f9
 const AUTH_URL = 'https://formbeta.yorktechapps.com'; // ... or the address to the instance of fbjs you wish to connect to
 
 //URL to take user back to after authentication
-const THIS_URL = 'http://172.16.3.135:3000/login'; // ... or whatever the address to your application is
+const THIS_URL = 'http://172.16.3.143:3000/login'; // ... or whatever the address to your application is
 
 const headers = [
     'id', 'name', 'color', 'code', 'number', 'code2',
